e63d525e48dc2a4c50d75823380915c7a70a8aae
nikhildarocha <niks77883@gmail.com>
2020-01-31 16:05:40 +0530

    pointers are fun lol

--- variables/pointers/pointers.go
+++ variables/pointers/pointers.go
@@ -6,12 +6,14 @@ import (
 )
 
 func main() {
-	name := "Nigel"
-	course := "Docker Deep Dive"
+	name := "Nikhil"
+	course := "GO Fundamentals"
 	module := 3.2
+	ptr := &module
 
 	fmt.Println("Name is", name, "and is of type", reflect.TypeOf(name))
 	fmt.Println("Module is", module, "and is of type", reflect.TypeOf(module))
 	fmt.Println("Course is", course, "and is of type", reflect.TypeOf(course))
 	fmt.Println("Memory address of *module* variable is", &module)
+	fmt.Println("Memory address of *module* variable is", ptr, "and the value of *module* is", *ptr)
 }
